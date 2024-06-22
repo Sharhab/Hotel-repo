ebd3e708ffa95e43142ac161f12dc540d6c61e10
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-06-22 21:24:43 +0100

    Create store.js

---
+++ frontend/src/components/store.js
@@ -0,0 +1,17 @@
+
+// store.js
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import userReducer from './reducers/userReducer';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
