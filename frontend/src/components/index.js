6c6cb0b0086865d8422d072bb89d63206038bf28
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-06-23 17:42:59 +0100

    Update index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import App from '../app';
+import App from '../App';
 import rootReducer from './store';
 
 // Create Redux store
