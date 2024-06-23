79e03a1b3431aabe3bb5162340890a1721d3e676
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-06-23 13:08:40 +0100

    Update store.js

--- frontend/src/components/store.js
+++ frontend/src/components/store.js
@@ -3,7 +3,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import userReducer from './reducers/userReducer';
+import userReducer from '../assets/userReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
