5b5ebfe9f9fa5d51f05f69f61e0d20e0784c0bb1
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-06-23 17:39:39 +0100

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 // components/Dashboard.js
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadUser } from '../layouts/userActions';
+import { loadUser } from '../layouts/userAction';
 
 const Dashboard = () => {
   const dispatch = useDispatch();
