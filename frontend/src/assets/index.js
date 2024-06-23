1620e83e3f59d54c175216588c9036b5fbf3a458
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-06-23 19:53:23 +0100

    Update index.js

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,9 +1,11 @@
 // src/reducers/index.js
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
+import taskReducer from './taskReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  tasks: taskReducer,
 });
 
 export default rootReducer;
